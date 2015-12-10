732df8526af27acb694a2adfb5e6120a7b2698c8
Steven Mortimer <reportmort@gmail.com>
2015-12-09 19:09:50 -0500

    Fix Error in Javascript
    The app javascript wasn't working because there was an extra double quote

--- inst/www/app.js
+++ inst/www/app.js
@@ -4,22 +4,22 @@ $(function(){
 	$("#csvfile").on("change", function loadfile(e){
 		if(!$("#csvfile").val()) return;
 		$("#outputcsv").addClass("hide").attr("href", "");
-		$(".spinner").show()
+		$(".spinner").show();
 		var req = ocpu.call("pothole_predict", {
 			input : $("#csvfile")[0].files[0]
 		}, function(tmp){
-			$("#outputcsv").removeClass("hide").attr("href", tmp.getLoc() + "R/.val/csv")
+			$("#outputcsv").removeClass("hide").attr("href", tmp.getLoc() + "R/.val/csv");
 		}).fail(function(){
-			alert(req.responseText)
+			alert(req.responseText);
 		}).always(function(){
-			$(".spinner").hide()
+			$(".spinner").hide();
 		});
 	});
 
 	//update the example curl line with the current server
 	$("#curlcode").text(
 		$("#curlcode").text().replace(
-			"https://{your_opencpu_server_address}/pothole/R/pothole_predict/json"", 
+			"https://{your_opencpu_server_address}/pothole/R/pothole_predict/json", 
 			window.location.href.match(".*/pothole/")[0] + "R/pothole_predict/json"
 		)
 	);
