a37daf82695d7497589cf7c07152901fc479cccb
Steven Mortimer <reportmort@gmail.com>
2015-12-09 19:45:35 -0500

    Fix javascript

--- inst/www/app.js
+++ inst/www/app.js
@@ -1,25 +1,24 @@
 $(document).ready(function () {
-
-	//csv file scoring
-	$("input.file").change(function () {
-		if(!$("input.file").val()) return;
-		$("#outputcsv").addClass("hide").attr("href", "");
-		$(".spinner").show();
-		var req = ocpu.call("pothole_predict", {
-			input : $("input.file")[0].files[0]
-		}, function(tmp){
-			$("#outputcsv").removeClass("hide").attr("href", tmp.getLoc() + "R/.val/csv");
-		}).fail(function(){
-			alert(req.responseText);
-		}).always(function(){
-			$(".spinner").hide();
-		});
-	});
-
 	//update the example curl line with the current server
   document.body.innerHTML = document.body.innerHTML.replace("https://{your_opencpu_server_address}/pothole/R/pothole_predict/json", 
 		window.location.href.match(".*/pothole/")[0] + "R/pothole_predict/json"
 	);
 	
 });
+
+function csvScore() {
+	//csv file scoring
+	if(!$("#csvfile").val()) return;
+	$("#outputcsv").addClass("hide").attr("href", "");
+	$(".spinner").show();
+	var req = ocpu.call("pothole_predict", {
+		input : $("#csvfile")[0].files[0]
+	}, function(tmp){
+		$("#outputcsv").removeClass("hide").attr("href", tmp.getLoc() + "R/.val/csv");
+	}).fail(function(){
+		alert(req.responseText);
+	}).always(function(){
+		$(".spinner").hide();
+	});
+}
 
